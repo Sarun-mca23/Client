d97d3503e8d1cdf5bdab54b60af1fd859679c648
Sarunkumar <139734908+Sarun-mca23@users.noreply.github.com>
2024-03-22 21:01:16 +0530

    completed

---
+++ src/home.js
@@ -0,0 +1,16 @@
+export default function Home(){
+    return(
+        <>
+        <div id="home">
+         <h3> <span id="span-1">Welcome to our </span>-<span id="span-2">Tamil Nadu Bank</span></h3>
+         <h4>"We have online banking system"</h4>
+         <p> 
+Available 24/7 from any device with an internet connection</p>
+          <p>Create a new account, balance checks and deposit your money.</p>
+
+        </div>
+        
+       
+        </>
+    )
+}
